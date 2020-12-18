0ee88d8f68d2cae514604ba8b93195195e50d8c4
Arllette Fonseca <arllette.fonseca@gmail.com>
2020-12-18 15:06:53 -0800

    completed vowel-count

---
+++ main.js
@@ -0,0 +1,16 @@
+function getCount(str) {
+  let vowelsCount = 0;
+  const vowels = ["a", "e", "i", "o", "u"];
+
+  for(let i = 0; i <= str.length - 1; i++) {
+    for(let j = 0; j<vowels.length -1; j++) {
+      if(str[i] === vowels[j]) {
+        vowelsCount++
+      }
+    }
+  }
+  return vowelsCount;
+}
+
+getCount("abracadabra");
+getCount("Hello World");
