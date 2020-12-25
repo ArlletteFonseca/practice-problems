df3b361daffb451373386d86b31062de47be8136
ArlletteFonseca <arllette.fonseca@gmail.com>
2020-12-25 01:10:41 +0000

    completed equivalent-arrays

---
+++ main.js
@@ -0,0 +1,15 @@
+var arrayStringsAreEqual = function (word1, word2) {
+  const one = word1.join('');
+  const two = word2.join('');
+  if (one === two) {
+    console.log(true)
+    return true;
+  } else {
+    console.log(false);
+    return false;
+  }
+};
+
+arrayStringsAreEqual(["ab", "c"],["a", "bc"])
+arrayStringsAreEqual(["a", "cb"], ["ab", "c"])
+arrayStringsAreEqual(["abc", "d", "defg"], ["abcddefg"])
