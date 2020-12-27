e35f1d62ace7d05c8a92eed67d6f0c4058015f9a
ArlletteFonseca <arllette.fonseca@gmail.com>
2020-12-27 04:01:37 +0000

    completed defangin-ip

---
+++ main.js
@@ -0,0 +1,17 @@
+var defangIPaddr = function (address) {
+  const separated = address.split('')
+  for(let i = 0; i <= separated.length - 1; i++) {
+    if(separated[i] === '.') {
+      separated[i] = '[.]';
+    }
+  }
+
+  return separated.join('');
+};
+
+
+defangIPaddr("1.1.1.1");
+//Output: "1[.]1[.]1[.]1"
+
+defangIPaddr("255.100.50.0");
+//Output: "255[.]100[.]50[.]0"
